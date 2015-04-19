80c39592c7639a785c19061a51213338f412cc2f
Martin Obel Thomsen <s134862@student.dtu.dk>
2015-04-19 17:26:50 +0200

    added Node.py and Eureka.py

--- Node.py
+++ Node.py
@@ -1,9 +1,13 @@
+import math
 class Node:
 
-
+    neighbours = []
+    dist_from_start = 0
+    dist_estimated_to_finish = 0
 
     def __init__(self):
         self.x
         self.y
 
-    def
+    def calc_estimated_dist(self,node):
+        math.sqrt(self.x*node.x+self.y*node.y)
