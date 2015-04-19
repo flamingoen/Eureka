19e8dd53ef3b56a95480c0037bcd45a30be0fb78
Martin Obel Thomsen <s134862@student.dtu.dk>
2015-04-19 18:39:31 +0200

    update node

--- Node.py
+++ Node.py
@@ -1,13 +1,18 @@
 import math
+
+
 class Node:
 
     neighbours = []
     dist_from_start = 0
     dist_estimated_to_finish = 0
 
-    def __init__(self):
-        self.x
-        self.y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-    def calc_estimated_dist(self,node):
-        math.sqrt(self.x*node.x+self.y*node.y)
+    def calc_estimated_dist(self, node):
+        self.dist_estimated_to_finish = math.sqrt(self.x*node.x+self.y*node.y)
+
+    def add_neighbour(self, node):
+        self.neigbours.append(node)
