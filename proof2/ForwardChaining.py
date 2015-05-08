ab216f5e961d59daa03a3ab5fa7d0e59df938c5c
Martin Obel Thomsen <s134862@student.dtu.dk>
2015-05-08 11:19:04 +0200

    just done some things

--- proof2/ForwardChaining.py
+++ proof2/ForwardChaining.py
@@ -21,24 +21,33 @@ def createKB():
 
     KB.append(Clause([Litteral("d")]))
 
+
+def forwardChaining(node):
+    foundClauses = []
+
+    # finding clause matching node
+    for clause in node.KB:
+        for litteral in node.litterals:
+            if clause.contains(litteral):
+                foundClauses.append(clause)
+
+    # make new node, check if branch is done or continue
+    for clause in foundClauses:
+        new = clause.reduced(node)
+        new.KB = [clause for clause in node.KB if clause not in foundClauses] # laver en ny kb af start.KB hvor de fundet clauses i newClauses er fjernet - virker ikke da to clauses godt kan være ens med med forskellig id
+
+        print new
+
+        if not new.KB:
+            return True
+        if new.KB == node.KB:
+            return False
+
+        forwardChaining(new)
+
+# sets up base case
 createKB()
-
-pool = []
-
 start = Clause([contradiction])
 start.KB = KB
 
-
-foundClauses = []
-for clause in start.KB:
-    for litteral in start.litterals:
-        if clause.contains(litteral):
-            foundClauses.append(clause)
-
-for clause in foundClauses:
-    new = clause.reduced(start)
-    new.KB = [x for x in start.KB if x not in foundClauses]
-    pool.append(new)
-
-for p in pool:
-    print p
+print forwardChaining(start)
