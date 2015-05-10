b6c34495aa246159d9fbbfd338666b074407dde1
Kaas <alexkaas2@gmail.com>
2015-05-10 18:43:33 +0200

    Does not work with the added "a"

--- proof2/ForwardChaining.py
+++ proof2/ForwardChaining.py
@@ -17,10 +17,36 @@ def createKB():
 
     KB.append(Clause([Litteral("b"), Litteral("d", False)]))
 
-    KB.append(Clause([Litteral("d")]))
+    KB.append(Clause([Litteral("c")]))
 
     KB.append(Clause([Litteral("d")]))
 
+    KB.append(Clause([Litteral("a")]))
+
+
+def createKB2():
+    #Actural KB
+    KB.append(Clause([Litteral("water"), Litteral("ok-pump", False)]))
+    KB.append(Clause([Litteral("water"), Litteral("on-pump", False)]))
+    KB.append(Clause([Litteral("water"), Litteral("man-fill", False)]))
+    KB.append(Clause([Litteral("man-fill"), Litteral("on-pump")]))
+    KB.append(Clause([Litteral("man-fill"), Litteral("on-pump")]))
+    KB.append(Clause([Litteral("water", False), Litteral("steam")]))
+    KB.append(Clause([Litteral("ok-boiler", False), Litteral("steam")]))
+    KB.append(Clause([Litteral("on-boiler", False), Litteral("steam")]))
+    KB.append(Clause([Litteral("steam", False), Litteral("water", False)]))
+    KB.append(Clause([Litteral("water", False), Litteral("steam")]))
+    KB.append(Clause([Litteral("on-boiler"), Litteral("steam", False)]))
+    KB.append(Clause([Litteral("ok-boiler"), Litteral("steam", False)]))
+    KB.append(Clause([Litteral("steam", False), Litteral("hot-drink")]))
+    KB.append(Clause([Litteral("coffee", False), Litteral("hot-drink")]))
+    KB.append(Clause([Litteral("steam", False), Litteral("hot-drink")]))
+    KB.append(Clause([Litteral("tea", False), Litteral("hot-drink")]))
+    KB.append(Clause([Litteral("coffee"), Litteral("tea")]))
+
+    #contradiction
+    KB.append(Clause([Litteral("ok-pump"), Litteral("hot-drink", False)]))
+    KB.append(Clause([Litteral("on-pump"), Litteral("hot-drink", False)]))
 
 def forwardChaining(node):
     foundClauses = []
@@ -34,7 +60,7 @@ def forwardChaining(node):
     reducedFoundClauses=[]
     for temp in foundClauses:
         reducedNode = temp.reduced(node)
-        if  reducedNode != node:
+        if  reducedNode != node and reducedNode != "":
             reducedFoundClauses.append(reducedNode)
     counter = -1
     for clause in reducedFoundClauses:
@@ -60,7 +86,7 @@ def forwardChaining(node):
         for clause1 in node.KB:
             addClause=True
             for clause2 in foundClauses:
-                if clause1 == clause2 or clause1 in new.KB:
+                if clause1 == clause2 or clause1 in new.KB or clause1 == new:
                     addClause = False
             if addClause:
                 new.KB.append(clause1)
@@ -80,6 +106,7 @@ def forwardChaining(node):
 
         if forwardChaining(new):
             return True
+
     return False
 # sets up base case
 createKB()
